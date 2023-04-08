aca8065a3968ed2c9b1f273c5557169b9b41d93d
Azibek44 <azibektasbolotuulu@gmail.com>
2023-04-08 18:40:17 +0600

    hm3

--- blog/tests.py
+++ blog/tests.py
@@ -1,41 +1,21 @@
 from django.test import TestCase,Client
 from django.urls import reverse
-
+# Create your tests here.
 class HelloViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-
-    def test_hello(self):
-            response = self.client.get(reverse("index-page"))
-            self.assertTemplateNotUsed(response,"blog/index.html") 
-            self.assertEqual(500,response.status_code)
+    
+    def test_index(self):
+        response = self.client.get(reverse("index-page"))
+        self.assertTemplateUsed(response,"blog/index.html")
+        self.assertEqual(200, response.status_code)
 
     def test_about(self):
         response = self.client.get(reverse("about-view"))
-        self.assertTemplateNotUsed(response,"blog/about.html")
-        self.assertEqual(500,response.status_code)
-
+        self.assertTemplateUsed(response,"blog/about.html")    
+        self.assertEqual(200, response.status_code)
+    
     def test_contacts(self):
-        response = self.client.get(reverse("contacts-view")) 
-        self.assertTemplateNotUsed(response,"blog/contacts.html")
-        self.assertEqual(500,response.status_code)
-
-    # def test_hello(self):
-    #     response = self.client.get(reverse("index-page"))
-    #     excepted_data = "Hello" 
-    #     self.assertEqual(excepted_data,response.content.decode())  
-    #     self.assertEqual(500,response.status_code)
-    #     self.assertEqual(response['name'],'alex')
-
-    # def test_about(self):
-    #     response = self.client.get(reverse("about-view"))
-    #     excepted_data = "white" 
-    #     self.assertTemplateNotUsed(response,blog/)
-    #     self.assertEqual(500,response.status_code)
-
-    # def test_contacts(self):
-    #     response = self.client.get(reverse("contacts-view"))
-    #     excepted_data = "black" 
-    #     self.assertEqual(excepted_data,response.content.decode())  
-    #     self.assertEqual(500,response.status_code)
-       
+        response = self.client.get(reverse("contacts-view"))
+        self.assertTemplateUsed(response,"blog/contacts.html")
+        self.assertEqual(200, response.status_code)
