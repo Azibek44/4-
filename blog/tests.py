9eb0726ad901f471ee634a5918b81693918eb5a5
Azibek44 <azibektasbolotuulu@gmail.com>
2023-04-08 18:20:34 +0600

    hm3

--- blog/tests.py
+++ blog/tests.py
@@ -4,20 +4,38 @@ from django.urls import reverse
 class HelloViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
+
     def test_hello(self):
-        response = self.client.get(reverse("hello-view"))
-        excepted_data = "Hello" 
-        self.assertEqual(excepted_data,response.content.decode())  
-        self.assertEqual(500,response.status_code)
-        self.assertEqual(response['name'],'alex')
+            response = self.client.get(reverse("index-page"))
+            self.assertTemplateNotUsed(response,"blog/index.html") 
+            self.assertEqual(500,response.status_code)
 
     def test_about(self):
         response = self.client.get(reverse("about-view"))
-        excepted_data = "white" 
-        self.assertEqual(excepted_data,response.content.decode())  
+        self.assertTemplateNotUsed(response,"blog/about.html")
+        self.assertEqual(500,response.status_code)
 
     def test_contacts(self):
-        response = self.client.get(reverse("contacts-view"))
-        excepted_data = "black" 
-        self.assertEqual(excepted_data,response.content.decode())  
+        response = self.client.get(reverse("contacts-view")) 
+        self.assertTemplateNotUsed(response,"blog/contacts.html")
+        self.assertEqual(500,response.status_code)
+
+    # def test_hello(self):
+    #     response = self.client.get(reverse("index-page"))
+    #     excepted_data = "Hello" 
+    #     self.assertEqual(excepted_data,response.content.decode())  
+    #     self.assertEqual(500,response.status_code)
+    #     self.assertEqual(response['name'],'alex')
+
+    # def test_about(self):
+    #     response = self.client.get(reverse("about-view"))
+    #     excepted_data = "white" 
+    #     self.assertTemplateNotUsed(response,blog/)
+    #     self.assertEqual(500,response.status_code)
+
+    # def test_contacts(self):
+    #     response = self.client.get(reverse("contacts-view"))
+    #     excepted_data = "black" 
+    #     self.assertEqual(excepted_data,response.content.decode())  
+    #     self.assertEqual(500,response.status_code)
        
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# get_about(request) - about/ -> "About"
-# get_contacts(request) - contacts/ -> "Contacts"
-def get_hello(request):
-    # request -> Объект класса HttpRequest, который формируется из запроса от клиента на наш сервер
-    # my_list = [1, 2, 3]
-    #     heading = """<!DOCTYPE html>
-    # <html lang="ru">
-    # <head>
-    #   <meta charset="UTF-8">
-    #   <title>Базовая разметка HTML</title>
-    # </head>
-    # <body>
-    #   <h1>Code Basics</h1>
-    #   <p>Бесплатные уроки по программированию и HTML для новичков</p>
-    # </body>
-    # </html>
-    #                  """
-
-    return HttpResponse("Hello", headers={"Name": "Alex"}, status=500)
-
+def get_index(request):
+     context = {
+           "title": "Главная страница",
+        "my_list": [1, 2, 3, 4]
+     }
+     return render(request, "blog/index.html", context=context)
+    
 def get_about(request):
-    return HttpResponse("white")
+    context = {
+         "title": "Страница о нас"
+    }
+    return render(request, "blog/about.html", context=context)
 
 def get_contacts(request):
-    return HttpResponse("black")
+    context = {
+       "title": "Как с нами связаться"
+    }
+    return render(request, "blog/contacts.html", context=context)
