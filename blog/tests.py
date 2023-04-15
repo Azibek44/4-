71faa1609fc226379470bfe554ecf3b3bac10cd0
Azibek44 <azibektasbolotuulu@gmail.com>
2023-04-15 21:57:31 +0600

    HW5

--- blog/tests.py
+++ blog/tests.py
@@ -1,36 +1,32 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from django.urls import reverse
-# Create your tests here.
-class HelloViewTestCase(TestCase):
+
+
+class BlogViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-    
+
     def test_index(self):
         response = self.client.get(reverse("index-page"))
-        self.assertTemplateUsed(response,"blog/index.html")
+        self.assertTemplateUsed(response, "blog/index.html")
         self.assertEqual(200, response.status_code)
 
     def test_about(self):
         response = self.client.get(reverse("about-view"))
-        self.assertTemplateUsed(response,"blog/about.html")    
+        self.assertTemplateUsed(response, "blog/about.html")
         self.assertEqual(200, response.status_code)
-    
+
     def test_contacts(self):
         response = self.client.get(reverse("contacts-view"))
-        self.assertTemplateUsed(response,"blog/contacts.html")
+        self.assertTemplateUsed(response, "blog/contacts.html")
         self.assertEqual(200, response.status_code)
 
-    def test_post(self):
-        response = self.client.get(reverse("post-view"))
-        self.assertTemplateUsed.get(response,"blog/post.html")  
-        self.assertEqual(200, response.status_code)  
+    def test_post_update(self):
+        response = self.client.get(reverse("post-update"))
+        self.assertTemplateUsed("blog/post_update.html")
+        self.assertEqual(200, response.status_code)
 
-    def test_create(self):
-        response = self.client.get(reverse("create-view"))
-        self.assertTemplateUsed(response,"blog/create.html")    
-        self.assertEqual(200, response.status_code)  
-
-    def test_uptade(self):
-        response = self.client.get(reverse("uptade-view"))
-        self.assertTemplateUsed(response,"blog/uptade.html")    
-        self.assertEqual(200, response.status_code)      
+    def test_post_create(self):
+        response = self.client.get(reverse("post-create"))
+        self.assertTemplateUsed("blog/post_create.html")
+        self.assertEqual(200, response.status_code)
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from blog import views
 
-urlpatterns = [ 
-    path("", views.get_index, name = "index-page"),
-    path("about/",views.get_about, name = "about-view"),
-    path("contacts/",views.get_contacts, name = "contacts-view"),
-    path("post/", views.get_post, name="post-detail"),
-    path("create/", views.post_create, name = "create-view"),
-    path("uptade/", views.post_update, name = "uptade-view")
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index-page"),
+    path("about/", views.AboutView.as_view(), name="about-view"),
+    path("contacts/", views.ContactsView.as_view(), name="contacts-view"),
+    path("post/<int:pk>", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/update/<int:pk>", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/create", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/delete", views.PostDeleteConfirimView.as_view(), name="post-delete-confirim")
 ]
